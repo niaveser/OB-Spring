716e92a5a43bf2b78c3627073631b2b9385c9ad6
niaveser <94000360+niaveser@users.noreply.github.com>
2021-12-14 18:18:22 +0100

    Add files via upload

---
+++ spring sesiones 4 5 6/spring-sesiones-4-5-6/src/test/java/com/example/springsesiones456/SpringSesiones456ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsesiones456;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSesiones456ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
